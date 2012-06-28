e1c95d89f50812342c8819c983cc581463f47067
Ralph Hoch <ralph.hoch@tuwien.ac.at>
2012-06-28 16:34:26 +0200

    pacman now consumes points

--- src/at/ac/foop/pacman/application/client/GameController.java
+++ src/at/ac/foop/pacman/application/client/GameController.java
@@ -144,7 +144,7 @@ public class GameController extends Observable implements IGame {
 		// TODO: This players are only placeholders for the real players
 		//       that get sent to the client once the game starts!
 		// logger.info("IN NOTIFY POSITIONS START");
-		this.playerOutput();
+		// this.playerOutput();
 		for(Long id : positions.keySet()) {
 			Player player = players.get(id.intValue() - 1);
 			Pacman pacman = player.getPacman();
@@ -154,6 +154,9 @@ public class GameController extends Observable implements IGame {
 			//Give the pacman the square on which is should be
 			((Field)map.getSquare(point.getX(), point.getY())).resetPlayers();
 			map.getSquare(point.getX(), point.getY()).enter(player);
+			if(pacman.getLocation() != null) {
+				pacman.getLocation().leave(player);
+			}
 			pacman.setLocation(map.getSquare(point.getX(), point.getY()));
 			pacman.setColor(PacmanColor.values()[id.intValue() - 1]);
 			//player.setPacman(pacman);
@@ -169,7 +172,7 @@ public class GameController extends Observable implements IGame {
 		this.setChanged();
 		this.notifyObservers();
 		this.clearChanged();
-		this.playerOutput();		
+		// this.playerOutput();		
 		// logger.info("IN NOTIFY POSITIONS END");
 		// this.playerOutput();
 	}
@@ -190,9 +193,9 @@ public class GameController extends Observable implements IGame {
 			player.setDirection(direction);
 		}
 		
-		this.playerOutput();
+		// this.playerOutput();
 		List<Square> squares = this.movePacmans();
-		this.playerOutput();
+		// this.playerOutput();
 		
 		for (Square square : squares) {
 			((Field)square).resolveConflict();
@@ -257,6 +260,7 @@ public class GameController extends Observable implements IGame {
 				} else {
 					currentSquare.leave(player);
 					nextSquare.enter(player);
+					player.addPoints(nextSquare.consumePoints());
 					player.getPacman().setLocation(nextSquare);
 					squares.add(nextSquare);
 				}
